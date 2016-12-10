6005ee028c34e9222f54f6b0bc5027159157b630
Rens van Woudenberg <rensvanw@outlook.com>
2016-12-10 19:17:33 +0100

    First commit

---
+++ config.js
@@ -0,0 +1,16 @@
+var config = {};
+
+config.debug = process.env.DEBUG || false;
+
+config.mqtt = {};
+config.mqtt.namespace = process.env.MQTT_NAMESPACE || 'legoHouseWoonkamerOutput';
+config.mqtt.hostname = process.env.MQTT_HOSTNAME || 'test.mosquitto.org';
+config.mqtt.port = process.env.MQTT_PORT || 1883;
+
+config.mongodb = {};
+config.mongodb.hostname = process.env.MONGODB_HOSTNAME || 'admin:xENWIbiNIMBZ0xi@ds119548.mlab.com';
+config.mongodb.port = process.env.MONGODB_PORT || 19548;
+config.mongodb.database = process.env.MONGODB_DATABASE || 'legohouse';
+config.mongodb.collection = process.env.MONGODB_COLLECTION || 'woonkamer';
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,43 @@
+/**
+ *
+ * This NodeJS application listens to MQTT messages and records them to MongoDB
+ *
+ * @author  Dennis de Greef <github@link0.net>
+ * @license MIT
+ *
+ */
+var mongodb = require('mongodb');
+var mqtt = require('mqtt');
+var config = require('./config');
+
+var mqttUri = 'mqtt://' + config.mqtt.hostname + ':' + config.mqtt.port;
+var client = mqtt.connect(mqttUri);
+
+client.on('connect', function() {
+    client.subscribe(config.mqtt.namespace);
+});
+
+var mongoUri = 'mongodb://' + config.mongodb.hostname + ':' + config.mongodb.port + '/' + config.mongodb.database;
+mongodb.MongoClient.connect(mongoUri, function(error, database) {
+    if (error != null) {
+        throw error;
+    }
+    var collection = database.collection(config.mongodb.collection);
+    collection.createIndex({ "topic": 1 });
+
+    client.on('message', function(topic, message) {
+        let fixedMessage = JSON.parse(message.toString().replace(/\'/g, '\"'));
+        let messageObject = {
+            lamp: fixedMessage.lamp,
+            verwarming: fixedMessage.verwarming,
+            bewegingssensor: fixedMessage.bewegingssensor,
+            tempsensor: fixedMessage.tempsensor,
+            timestamp: Date.now()
+        };
+        collection.insert(messageObject, function(error, result) {
+            if (error != null) {
+                console.log("ERROR: " + error);
+            }
+        });
+    });
+});
