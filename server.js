8c87ec494a13f126b6b45ddf404d1a1517a63c99
Rens van Woudenberg <rensvanw@outlook.com>
2016-12-11 19:48:41 +0100

    changed variable names

--- server.js
+++ server.js
@@ -6,18 +6,18 @@
  * @license MIT
  *
  */
-var mongodb = require('mongodb');
-var mqtt = require('mqtt');
-var config = require('./config');
+const mongodb = require('mongodb');
+const mqtt = require('mqtt');
+const config = require('./config');
 
-var mqttUri = 'mqtt://' + config.mqtt.hostname + ':' + config.mqtt.port;
-var client = mqtt.connect(mqttUri);
+const mqttUri = 'mqtt://' + config.mqtt.hostname + ':' + config.mqtt.port;
+const client = mqtt.connect(mqttUri);
 
 client.on('connect', function() {
     client.subscribe(config.mqtt.namespace);
 });
 
-var mongoUri = 'mongodb://' + config.mongodb.hostname + ':' + config.mongodb.port + '/' + config.mongodb.database;
+const mongoUri = 'mongodb://' + config.mongodb.hostname + ':' + config.mongodb.port + '/' + config.mongodb.database;
 mongodb.MongoClient.connect(mongoUri, function(error, database) {
     if (error != null) {
         throw error;
