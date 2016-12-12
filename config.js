45da55f577059ea87a3be9f81cfeb00efe68dd38
Rens van Woudenberg <rensvanw@outlook.com>
2016-12-12 22:18:46 +0100

    changed mqtt server

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ config.debug = process.env.DEBUG || false;
 
 config.mqtt = {};
 config.mqtt.namespace = process.env.MQTT_NAMESPACE || 'legoHouseWoonkamerOutput';
-config.mqtt.hostname = process.env.MQTT_HOSTNAME || 'test.mosquitto.org';
+config.mqtt.hostname = process.env.MQTT_HOSTNAME || '192.168.1.7';
 config.mqtt.port = process.env.MQTT_PORT || 1883;
 
 config.mongodb = {};
 config.mongodb.hostname = process.env.MONGODB_HOSTNAME || 'admin:xENWIbiNIMBZ0xi@ds119548.mlab.com';
 config.mongodb.port = process.env.MONGODB_PORT || 19548;
 config.mongodb.database = process.env.MONGODB_DATABASE || 'legohouse';
-config.mongodb.collection = process.env.MONGODB_COLLECTION || 'woonkamer';
+config.mongodb.collection = process.env.MONGODB_COLLECTION || 'livingroomdatapoints';
 
 module.exports = config;
